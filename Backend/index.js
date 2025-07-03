c4dd2c1cff00b32e9d8241f30b28030d1ca52c90
Harsh sandhu <sandhuharsh9466@gmail.com>
2025-07-03 12:41:38 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://pro-near-me.vercel.app'],
+  origin: ['http://localhost:3000', 'https://pro-near-me-an2r.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
